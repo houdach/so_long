471dab03b48b8847877a465e63ab48bbc313e6e5
Houda Chouai <hchouai@e1r11p3.1337.ma>
2023-12-27 15:47:40 +0100

    maps validation

--- map.c
+++ map.c
@@ -1,5 +1,6 @@
 #include "map.h"
 #include "get_next_line.h"
+
 size_t	ft_strlen1(char *s)
 {
 	size_t	i;
@@ -12,25 +13,29 @@ size_t	ft_strlen1(char *s)
 int validate_map(int fd)
 {
     int i;
-    char *line1;
     char *line;
     int arr[2];
     size_t len;
+    size_t lens;
+
     arr[0] = 0;
     arr[1] = 0;
     fd = open("mapp.ber", O_RDONLY);
-    line1 = get_next_line(fd);
+    line = get_next_line(fd);
     i = 0;
-    while (line1[i] != '\0')
+    while (line[i] != '\0' && line[i] != '\n')
     {
-        if (line1[i] != '1')
-            return(0);
-        else
+        if (line[i] == '1')
             i++;
+        else
+            return (0);
     }
+    lens = ft_strlen1(line);
     while ((line = get_next_line(fd)) != NULL)
     {
         len = ft_strlen1(line);
+        if(len != lens)
+            return (0);
         if (line[0] != '1' || line[len - 1] != '1')
             break ;
         i = 0;
@@ -62,37 +67,24 @@ int read_map()
     char *line;
     int fd;
     int br;
+
     fd = open("mapp.ber", O_RDONLY);
-    // line = get_next_line(fd);
-    br = 0;
     int flag;
-    //size_t len;
     flag = validate_map(fd);
+    br = 0;
     if (flag == 1)
     {
          while ((line = get_next_line(fd)) != NULL)
         {
-            // len = ft_strlen1(line);
-            // printf("%d\n", len);
             br += printf("%s", line);
             free(line);
-            // fd = open("houda.txt", O_CREAT | O_RDONLY, 0777);
         }
     }
     return (br);
-} line = get_next_line(fd);
-    i = 0;
-    while (line[i] != '\0')
-    {
-        if (line[i] != '1')
-            return(0);
-        else
-            i++;
-    }
+}
 
 int main()
 {
     int cn;
     cn = read_map();
-    //printf("%d\n", cn);
 }
